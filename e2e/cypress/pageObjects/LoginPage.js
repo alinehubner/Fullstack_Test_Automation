1e737833d5d72faaa45eee5577bc80cc0c496809
Aline Hubner <proceditec@gmail.com>
2026-01-18 21:36:34 -0300

    Fix login reuse with CI env vars

--- e2e/cypress/pageObjects/LoginPage.js
+++ e2e/cypress/pageObjects/LoginPage.js
@@ -1,26 +1,32 @@
 export class LoginPage {
-    visit() {
-      cy.visit("/");
-    }
-  
-    preencherUsuario(usuario) {
-      cy.get('[data-test="username"]').clear().type(usuario);
-    }
-  
-    preencherSenha(senha) {
-      cy.get('[data-test="password"]').clear().type(senha);
-    }
-  
-    clicarLogin() {
-      cy.get('[data-test="login-button"]').click();
-    }
-
-    loginComUsuarioValido() {
-      this.visit();
-      this.preencherUsuario(Cypress.env("user"));
-      this.preencherSenha(Cypress.env("pass"));
-      this.clicarLogin();
-    }
-    
+  visit() {
+    cy.visit("/");
   }
+
+  preencherUsuario(usuario) {
+    cy.get('[data-test="username"]').clear().type(usuario);
+  }
+
+  preencherSenha(senha) {
+    cy.get('[data-test="password"]').clear().type(senha, { log: false });
+  }
+
+  clicarLogin() {
+    cy.get('[data-test="login-button"]').click();
+  }
+
+  loginComUsuarioValido() {
+    const username = Cypress.env("USERNAME");
+    const password = Cypress.env("PASSWORD");
+
+    this.visit();
+    this.preencherUsuario(username);
+    this.preencherSenha(password);
+    this.clicarLogin();
+
+    // garante que o login concluiu antes de continuar
+    cy.contains("Products", { timeout: 15000 }).should("be.visible");
+  }
+}
+
   
